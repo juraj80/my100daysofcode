4459637f46e8ad3ff1dbc1264005ae3be1930255
juraj klucka <juraj.klucka@gmail.com>
2019-01-05 13:39:53 +0100

    used collections (counter, namedtuple) on movie set

--- days/04-06-collections/directors-template.py
+++ days/04-06-collections/directors-template.py
@@ -7,6 +7,11 @@ movie_data = 'https://raw.githubusercontent.com/pybites/challenges/solutions/13/
 movies_csv = 'movies.csv'
 urlretrieve(movie_data, movies_csv)
 
+# csv structure:
+# color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes,actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes
+# Color,James Cameron,723,178,0,855,Joel David Moore,1000,760505847,Action|Adventure|Fantasy|Sci-Fi,CCH Pounder,Avatar ,886204,4834,Wes Studi,0,avatar|future|marine|native|paraplegic,http://www.imdb.com/title/tt0499549/?ref_=fn_tt_tt_1,3054,English,USA,PG-13,237000000,2009,936,7.9,1.78,33000
+# Color,Gore Verbinski,302,169,563,1000,Orlando Bloom,40000,309404152,Action|Adventure|Fantasy,Johnny Depp,Pirates of the Caribbean: At World's End ,471220,48350,Jack
+
 Movie = namedtuple('Movie', 'title year score')
 
 def get_movies_by_director(data=movies_csv):
@@ -14,7 +19,7 @@ def get_movies_by_director(data=movies_csv):
         where keys are directors, and values is a list of movies (named tuples)'''
     directors = defaultdict(list)
     with open(data) as f: # The Pythonic way to open a file is to use a context manager ('with' statement)
-        for line in csv.DictReader(f): # to parse every line to OrderedDict
+        for line in csv.DictReader(f): # https://docs.python.org/3.4/library/csv.html - creates an object which operates like a regular reader csv.DictReader(csvfile, fieldnames=None, restkey=None, restval=None, dialect='excel', *args, **kwds). If the fieldnames parameter is omitted, the values in the first row of csvfile will be used as the fieldnames
             try:
                 director = line['director_name']
                 movie = line['movie_title'].replace('\xa0','')
@@ -30,6 +35,9 @@ def get_movies_by_director(data=movies_csv):
 
 def main():
     directors = get_movies_by_director()
+
+    print(directors)
+
 
     # lookup for Christopher Nolan
     print(directors['Christopher Nolan'])
