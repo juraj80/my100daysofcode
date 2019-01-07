8adf350808206ca878186666264fc19d632321dd
juraj klucka <juraj.klucka@gmail.com>
2019-01-07 13:28:03 +0100

    updating of exercise with data.py file

--- days/07-09-python-data-structures/exercises/pull_cities.py
+++ days/07-09-python-data-structures/exercises/pull_cities.py
@@ -29,7 +29,7 @@ def main():
 
     # replace the 15th key in the dictionary with the 28th item in the list
     item28 = list_of_states[27]
-    key15= states_keys[14]
+    key15 = states_keys[14]
 
     dict_of_states[item28[0]] = dict_of_states[key15]
     del dict_of_states[key15]
