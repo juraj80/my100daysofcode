f8c37258b11c57a29c553f5c8b3f4848bc35909f
juraj klucka <juraj.klucka@gmail.com>
2019-01-16 10:35:08 +0100

    updated game logic

--- days/13-15-text-based-games-and-classes/rockpaper/program.py
+++ days/13-15-text-based-games-and-classes/rockpaper/program.py
@@ -1,4 +1,4 @@
-from rolls import Roll, Player
+from rolls import Scissors, Paper, Stone, Player
 import random
 
 def main():
@@ -7,8 +7,8 @@ def main():
 
     name = get_players_name()
 
-    player1 = Player(name)
-    player2 = Player("computer")
+    player1 = Player(name,0)
+    player2 = Player("computer",0)
 
     game_loop(player1,player2,rolls)
 
@@ -21,22 +21,24 @@ def print_header():
 
 
 def get_players_name():
-    name = input('Type your name here: ')
+    name = input('Type your name here, please: ')
     return name
 
 def build_the_three_rolls():
-    Scissors = Roll('Scissors')
-    Paper = Roll('Paper')
-    Stone = Roll('Stone')
-    return [Scissors,Paper,Stone]
+    r1 = Scissors('Scissors')
+    r2 = Paper('Paper')
+    r3 = Stone('Stone')
+    return [r1,r2,r3]
 
 def game_loop(p1,p2,rolls):
     count = 0
+    print(f"OK, let's play a game {p1.name}!!! ")
+    print()
 
     while count < 3:
-        p2_roll = random.choice(rolls) #TODO: get random roll
+        p2_roll = random.choice(rolls)
 
-        p1_roll = input("Your choice? Enter [s]tone, [p]aper, or s[c]issors ? ").lower() #TODO: have player choose a roll
+        p1_roll = input("Choose [s]tone, [p]aper, or s[c]issors: ").lower()
         if p1_roll == 's':
             p1_roll = rolls[2]
         elif p1_roll == 'p':
@@ -46,16 +48,33 @@ def game_loop(p1,p2,rolls):
         else:
             print('Wrong input !!!')
             break
-
-        print(f'Player {p1.name} throws {p1_roll.name}')
-        print(f'Computer throws {p2_roll.name}')
-#        outcome = p1_roll.can_defeat(p2_roll)
-
+        print()
         # display throws
+        print(f'{p1.name} throws {p1_roll.name}')
+        print(f'Computer throws {p2_roll.name}')
+        print()
+        outcome = p1_roll.can_defeat(p2_roll)
         # display winner for this round
 
+        if outcome == 'win':
+            p1.points+=1
+            print(f'{p1.name} wins round {count+1} and have {p1.points} points')
+        elif outcome == 'loose':
+            p2.points+=1
+            winner = 'Computer'
+            print(f'Computer wins round {count+1} and have {p2.points} points')
+        else:
+            print('Tie')
+            count-=1
+
         count += 1
-    # Compute who won
+        print()
+
+    # display winner for this round
+    if p1.points > p2.points:
+        print(f'{p1.name} won the whole game with {p1.points} points!!!')
+    else:
+        print(f'Computer won the whole game with {p2.points} points!!!')
 
 
 
--- days/13-15-text-based-games-and-classes/rockpaper/rolls.py
+++ days/13-15-text-based-games-and-classes/rockpaper/rolls.py
@@ -1,17 +1,39 @@
-
 class Roll:
     def __init__(self, name):
         self.name = name
 
-    def can_defeat(self,other_roll):
-        pass
 
+class Scissors(Roll):
+    def can_defeat(self, other_roll):
+        if other_roll.name == 'Stone':
+            return 'win'
+        elif other_roll.name == self.name:
+            return 'tie'
+        else:
+            return 'loose'
+
+
+class Paper(Roll):
+    def can_defeat(self, other_roll):
+        if other_roll.name == 'Stone':
+            return 'win'
+        elif other_roll.name == self.name:
+            return 'tie'
+        else:
+            return 'loose'
+
+
+class Stone(Roll):
+    def can_defeat(self, other_roll):
+        if other_roll.name == 'Scissors':
+            return 'win'
+        elif other_roll.name == self.name:
+            return 'tie'
+        else:
+            return 'loose'
 
 
 class Player:
-    def __init__(self ,name):
+    def __init__(self, name, points):
         self.name = name
-
-
-
-
+        self.points = points
