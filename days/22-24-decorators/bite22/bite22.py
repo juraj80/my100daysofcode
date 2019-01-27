8d85dd3d7ba7cc2bd25075bdc63373952b29bfe4
juraj klucka <juraj.klucka@gmail.com>
2019-01-27 13:26:08 +0100

    decorator that wraps text inside one or more html tags

--- days/22-24-decorators/bite22/bite22.py
+++ days/22-24-decorators/bite22/bite22.py
@@ -9,3 +9,22 @@ def get_text(text='I code with PyBites'):
     return text
 - would return: <p><strong>I code with PyBites</strong></p>
 '''
+
+from functools import wraps
+
+def make_html(element):
+    def decorator(func):
+        @wraps(func)
+        def wrapper():
+            return f'<{element}>{func()}</{element}>'
+        return wrapper
+    return decorator
+
+
+@make_html("p")
+@make_html("strong")
+def get_text(text='I code with PyBites'):
+    return text
+
+if __name__ == '__main__':
+    print(get_text())
