b1687a11264e7afcf1d5c1119c88aa9faab00aaa
juraj klucka <juraj.klucka@gmail.com>
2019-02-07 21:30:08 +0100

    csv data analysis

--- days/37-39-csv-data-analsys/drug_use_by_age_2012/research.py
+++ days/37-39-csv-data-analsys/drug_use_by_age_2012/research.py
@@ -16,13 +16,12 @@ Record = namedtuple('Record', 'age,n,alcohol_use,alcohol_frequency,marijuana_use
                               'tranquilizer_frequency,stimulant_use,stimulant_frequency,meth_use,meth_frequency,'
                               'sedative_use,sedative_frequency')
 
+
 def init():
     base_folder = os.path.dirname(__file__)
-    filename = os.path.join(base_folder,'data','drug-use-by-age.csv')
+    filename = os.path.join(base_folder, 'data', 'drug-use-by-age.csv')
 
-
-
-    with open(filename,'r',encoding='utf-8') as fin:
+    with open(filename, 'r', encoding='utf-8') as fin:
         reader = csv.DictReader(fin)
 
         data.clear()
@@ -35,12 +34,8 @@ def init():
 
     research_log.trace(f'Loading a CSV file {filename} and parsing of data successful')
 
+
 def parse_row(row):
-    for value in row.values():
-        if value == '-':
-            value = 0
-
-
     row['n'] = int(row['n'])
     row['alcohol-use'] = float(row['alcohol-use'])
     row['alcohol-frequency'] = float(row['alcohol-frequency'])
@@ -69,22 +64,31 @@ def parse_row(row):
     row['sedative-use'] = float(row['sedative-use'])
     row['sedative-frequency'] = float(row['sedative-frequency'])
 
-
-    record = Record(age=row['age'],n=row['n'],alcohol_use=row['alcohol-use'],alcohol_frequency=row['alcohol-frequency'],marijuana_use=row['marijuana-use'],marijuana_frequency=row['marijuana-frequency'],
-                    cocaine_use=row['cocaine-use'],cocaine_frequency=row['cocaine-frequency'],crack_use=row['crack-use'],crack_frequency=row['crack-frequency'],heroin_use=row['heroin-use'],
-                    heroin_frequency=row['heroin-frequency'],hallucinogen_use =row['hallucinogen-use'],hallucinogen_frequency=row['hallucinogen-frequency'],inhalant_use=row['inhalant-use'],
-                    inhalant_frequency=row['inhalant-frequency'],pain_releiver_use = row['pain-releiver-use'],pain_releiver_frequency=row['pain-releiver-frequency'],oxycontin_use=row['oxycontin-use'],
-                    oxycontin_frequency=row['oxycontin-frequency'],tranquilizer_use=row['tranquilizer-use'],tranquilizer_frequency=row['tranquilizer-frequency'],stimulant_use=row['stimulant-use'],
-                    stimulant_frequency=row['stimulant-frequency'],meth_use=row['meth-use'],meth_frequency=row['meth-frequency'],sedative_use=row['sedative-use'], sedative_frequency=row['sedative-frequency'])
+    record = Record(age=row['age'], n=row['n'], alcohol_use=row['alcohol-use'],
+                    alcohol_frequency=row['alcohol-frequency'], marijuana_use=row['marijuana-use'],
+                    marijuana_frequency=row['marijuana-frequency'],
+                    cocaine_use=row['cocaine-use'], cocaine_frequency=row['cocaine-frequency'],
+                    crack_use=row['crack-use'], crack_frequency=row['crack-frequency'], heroin_use=row['heroin-use'],
+                    heroin_frequency=row['heroin-frequency'], hallucinogen_use=row['hallucinogen-use'],
+                    hallucinogen_frequency=row['hallucinogen-frequency'], inhalant_use=row['inhalant-use'],
+                    inhalant_frequency=row['inhalant-frequency'], pain_releiver_use=row['pain-releiver-use'],
+                    pain_releiver_frequency=row['pain-releiver-frequency'], oxycontin_use=row['oxycontin-use'],
+                    oxycontin_frequency=row['oxycontin-frequency'], tranquilizer_use=row['tranquilizer-use'],
+                    tranquilizer_frequency=row['tranquilizer-frequency'], stimulant_use=row['stimulant-use'],
+                    stimulant_frequency=row['stimulant-frequency'], meth_use=row['meth-use'],
+                    meth_frequency=row['meth-frequency'], sedative_use=row['sedative-use'],
+                    sedative_frequency=row['sedative-frequency'])
 
     return record
+
 
 def highest_alcohol_use() -> List[Record]:
     return sorted(data, key=lambda r: r.alcohol_use, reverse=True)
 
 
 def highest_marijuana_use() -> List[Record]:
-    return sorted(data, key=lambda r: r.marijuana_use,reverse=True)
+    return sorted(data, key=lambda r: r.marijuana_use, reverse=True)
+
 
 def highest_cocaine_use() -> List[Record]:
     return sorted(data, key=lambda r: r.cocaine_use, reverse=True)
