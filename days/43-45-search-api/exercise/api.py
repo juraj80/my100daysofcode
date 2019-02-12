93c7a4db37370bb634dd6f7e4a7989b5bfa154be
juraj klucka <juraj.klucka@gmail.com>
2019-02-12 15:24:16 +0100

    consuming HTTP services

--- days/43-45-search-api/exercise/api.py
+++ days/43-45-search-api/exercise/api.py
@@ -1,12 +1,23 @@
+from typing import List
+
 import requests
+from collections import namedtuple
 
 
-def get_results_from_api(keyword:str):
+Episode = namedtuple('Episode', 'category, id, url, title, description')
+
+
+def get_results_from_api(keyword: str) -> List[Episode]:
     url = 'http://search.talkpython.fm/api/search?q={}'.format(keyword)
     resp = requests.get(url)
 
     resp.raise_for_status()
-    print(resp)
-    return resp.json()
+    result = resp.json()
+    episodes = []
 
+    for item in result['results']:
+        if item['category'] == 'Episode':
+            episodes.append(Episode(**item))
+
+    return episodes
 
--- days/43-45-search-api/exercise/program.py
+++ days/43-45-search-api/exercise/program.py
@@ -5,11 +5,11 @@ def main():
 
     keyword = input("What keyword to search for?  <ENTER WORDS>: ")
 
-    results = api.get_results_from_api(keyword)
-    print('There are {} matching results: '.format(len(results['results'])))
+    episodes = api.get_results_from_api(keyword)
+    print('There are {} matching results: '.format(len(episodes)))
 
-    for idx, item in enumerate(results['results'], 1):
-        print('{}. {}'.format(idx, item['title']))
+    for idx, item in enumerate(episodes, 1):
+        print('{}. {}'.format(idx, item.title))
 
 if __name__ == '__main__':
     main()
