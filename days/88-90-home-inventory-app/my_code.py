88e391684f31fd0fa24a0cd99e303d65e48237fd
juraj klucka <juraj.klucka@gmail.com>
2019-03-23 21:22:33 +0100

    simple CLI app with sqlite3 connection

--- days/88-90-home-inventory-app/my_code.py
+++ days/88-90-home-inventory-app/my_code.py
@@ -1,6 +1,22 @@
+import sqlite3
 import sys
+from contextlib import contextmanager
 
-ROOMS = {}
+DB = "inventory.db"
+
+def first_launch():
+    try:
+        conn = sqlite3.connect(DB)
+    except:
+        sys.exit('Error code X.')
+
+@contextmanager
+def access_db():
+    conn = sqlite3.connect(DB)
+    cursor = conn.cursor()
+    yield cursor
+    conn.commit()
+    conn.close()
 
 def main_menu():
     menu = {}
@@ -23,7 +39,7 @@ def main_menu():
         elif choice == '3':
             view_inventory(check_input())
         elif choice == '4':
-            calc_value(check_input())
+            calc_total()
         elif choice == '5':
             sys.exit()
         else:
@@ -35,41 +51,78 @@ def check_input():
         for room in list_rooms():
             print(room)
         selection = input('Select a room: ').lower()
+        print(list_rooms())
         if selection not in list_rooms():
             print('\n%s does not exist.' % selection)
         else:
-            return selection
+            return scrub(selection)
 
 def add_room():
-    name = input('Enter the name of new room: ').lower()
-    ROOMS[name] = {}
-    print(f"Room '{name}' added.")
+    name = input('Enter the name of new room: ')
+    name = scrub(name)
 
-def list_rooms():
-    return [room for room, items in ROOMS.items()]
+    with access_db() as cursor:
+        cursor.execute("CREATE TABLE'" + name.lower() + "'(Item TEXT, Value REAL)")
+
+        print("\nRoom with name %s was added to the db." % name)
+
+# def list_rooms():
+#     return [room for room, items in ROOMS.items()]
 
 
 def add_inventory(room):
-    item = input('Enter the name of item: ')
-    value = float(input('Enter the value of item: '))
-    ROOMS[room].update({item: value})
-    print(f"Item '{item}' with value {value} added to the inventory of room {room}")
+    while True:
+        print('\n')
+        name = input('Enter the name of item: ')
+        cost = float(input('Enter the value of item: '))
+        with access_db() as cursor:
+            cursor.execute("INSERT INTO '" + room + "' VALUES(?,?)", [name, cost])
+            print(f"Item '{name}' with value {cost} added to the inventory of room {room}")
+
+        cont = input('\nHit Q to quit or any other key to continue.')
+        if cont.lower() == 'q':
+            break
+
 
 def view_inventory(room):
     print(f'Items or room {room} :')
-    # print(ROOMS[room])
-    # print(type(ROOMS[room]))
-    for item, value in ROOMS[room].items():
-        print('item: %s, Value: %f' % (item, value))
-
-def calc_value(room):
     total = 0
-    for item, value in ROOMS[room].items():
-        total += value
-    print(f'Total value of room {room} is {total} ')
+    with access_db() as cursor:
+        cursor.execute("SELECT * FROM '" + room + "'")
+        for data in cursor:
+            print("   %s: $%d" % (data[0], data[1]))
+            total += data[1]
+        print("Total value of room %s: $%d" % (room, total))
+
+#Function to calculate the $ total of the entire database.
+def calc_total():
+    total = 0
+    room_list = list_rooms()
+    with access_db() as cursor:
+        for room in room_list:
+            cursor.execute("SELECT Value FROM '" + room + "'")
+            for value in cursor:
+                # print(value)
+                total += value[0]
+        print("Total value of all rooms: $%d" % total)
+
+
+def list_rooms():
+    room_list =[]
+    with access_db() as cursor:
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        for room in cursor:
+            room_list.append(room[0])
+    return room_list
+
+
+def scrub(name):
+    return ''.join( chr for chr in name if chr.isalnum())
+
 
 
 if __name__ == '__main__':
+    first_launch()
     main_menu()
 
 
